a275bba5b7aa0b06d91b91e3f1ede1f0260871c8
LeonardoSacra04 <oliveiraleo04@gmail.com>
2025-04-03 19:39:52 -0300

    recebe o salário dado pelo usuário e mostra o FGTS

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿decimal salario;
+decimal fgts;
+
+Console.Clear();
+
+Console.WriteLine("Digite qual é o seu salário: ");
+salario = Convert.ToDecimal(Console.ReadLine()!);
+
+Console.Clear();
+
+fgts = salario * 0.08m;
+
+Console.WriteLine($"Seu salário: {salario:C2}");
+Console.WriteLine($"FGTS: {fgts:C2}");
